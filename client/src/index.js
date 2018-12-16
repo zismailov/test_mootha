42d11762e1251a9d3aac5879d5ab0830324017b2
Zulkar <bedelbaev@gmail.com>
2018-12-16 22:43:03 +0600

    add frontend routes

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import * as serviceWorker from "./serviceWorker";
+import Movies from "./movies/Movies";
+import Movie from "./movies/Movie";
+// Import routing components
+import { BrowserRouter, Link, Route } from "react-router-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
+ReactDOM.render(
+  <BrowserRouter>
+    <div>
+      <Route exact path="/" component={Movies} />
+      <Route exact path="/movies/:id" component={Movie} />
+    </div>
+  </BrowserRouter>,
+  document.getElementById("root")
+);
 serviceWorker.unregister();
