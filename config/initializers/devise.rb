55df2580696c1269fa3810b3b277fdd131136ce1
Zulkar <bedelbaev@gmail.com>
2018-12-17 20:53:20 +0600

    update jwt configs

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -248,7 +248,7 @@ Devise.setup do |config|
   # should add them to the navigational formats lists.
   #
   # The "*/*" below is required to match Internet Explorer requests.
-  # config.navigational_formats = ['*/*', :html]
+  config.navigational_formats = ['*/*', :html]
 
   # The default HTTP method used to sign out a resource. Default is :delete.
   config.sign_out_via = :delete
@@ -284,7 +284,7 @@ Devise.setup do |config|
   config.jwt do |jwt|
     jwt.secret = ENV['DEVISE_JWT_SECRET_KEY']
     jwt.dispatch_requests = [
-      ['POST', %r{^/login$}]
+      ['POST', %r{^v1/users/sign_in$}]
     ]
     jwt.revocation_requests = [
       ['DELETE', %r{^/logout$}]
